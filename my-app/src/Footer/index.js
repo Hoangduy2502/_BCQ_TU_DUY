8761c6eb005bca2aab6b825cd84924e1b2064c99
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2021-09-18 12:38:33 +0700

    em

--- my-app/src/Footer/index.js
+++ my-app/src/Footer/index.js
@@ -3,14 +3,17 @@ import ContentFooter from './contentFooter';
 import Rating from './rating';
 import Data1 from '../contant/index';
 import { useState } from 'react';
-const Footer = ()=>{
-    const data=Data1.Data
-    console.log("TEST",data)
-    return(
-    <div className="Footer">
-        <Rating />
-        <ContentFooter DataTT={data}/>
-    </div>)
+const Footer = () => {
+    const data = Data1.Data
+    console.log("TEST", data)
+    return (
+        <div className="Footer">
+            <div style={{height: 0}}>
+                <Rating />
+                <ContentFooter DataTT={data} />
+            </div>
+
+        </div>)
 }
 
 export default Footer;
