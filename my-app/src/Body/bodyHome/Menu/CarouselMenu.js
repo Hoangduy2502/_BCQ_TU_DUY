91fa78d65689d57583c8c757a8d70c3d13535949
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2021-09-27 22:11:02 +0700

    meo

--- my-app/src/Body/bodyHome/Menu/CarouselMenu.js
+++ my-app/src/Body/bodyHome/Menu/CarouselMenu.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const CarouselMenu = (props) => {
-    const {list}=props
+    const { list } = props;
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      };
+      console.log('tumeo',list);
     return (
-      
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active d-flex ">
-                                    {list}
-                                </div>
-                                {/* <div class="carousel-item">
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active d-flex ">
+                    {list}
+                    
+                </div>
+                {/* <div class="carousel-item">
                                     <img src="..." class="d-block w-100" alt="..."/>
                                 </div>
                                 <div class="carousel-item">
                                     <img src="..." class="d-block w-100" alt="..."/>
                                 </div> */}
-                            </div>
-                            {/* <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            </div>
+            {/* <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
@@ -24,8 +36,8 @@ const CarouselMenu = (props) => {
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a> */}
-                        </div>
-      
+        </div>
+
     );
 }
 
